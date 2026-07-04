25f56ede2bb96e52a76591d3b8836c7dd9c90a43
Anshumaan <anshumaan.1611@gmail.com>
2026-07-04 19:49:31 +0530

    fix : change in structure and made it ind grade

---
+++ app/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
+import pinoHttp from "pino-http";
+import routes from "./routes"
+const app = express();
+app.use(helmet());
+app.use(cors())
+app.use(compression())
+app.use(express.json());
+app.use(pinoHttp())
+app.use("/", routes)
+export default app;
